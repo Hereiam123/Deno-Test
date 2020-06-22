a8a30f593efec3ab237549527ab4dbdc43f6e616
Brian De Maio <bdemaio1@gmail.com>
2020-06-22 19:36:27 -0400

    Divying up routes

--- server.ts
+++ server.ts
@@ -1,9 +1,8 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
 const port = 5000;
 
 const app = new Application();
-
-const router = new Router();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
