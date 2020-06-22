6f2768d512183f822cefd415ec8a178cd764e2ea
Brian De Maio <bdemaio1@gmail.com>
2020-06-22 19:32:01 -0400

    Server running

--- server.ts
+++ server.ts
@@ -3,6 +3,15 @@ const port = 5000;
 
 const app = new Application();
 
+const router = new Router();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.get("/api/v1/movies", ({ response }: { response: any }) => {
+  response.body = "Hello world";
+});
+
 console.log(`Server running on port ${port}`);
 
 await app.listen({ port: port });
