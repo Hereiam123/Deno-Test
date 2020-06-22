3f59bb4eb28b7f5d31c3b3de066233ab804b3f5d
Brian De Maio <bdemaio1@gmail.com>
2020-06-22 19:42:25 -0400

    Divying up routes

---
+++ products.ts
@@ -0,0 +1,6 @@
+import { Product } from "./types.ts";
+
+let products: Product[] = [
+  { id: "1", name: "Movie one", description: "This is movie one", rating: 9 },
+  { id: "2", name: "Movie two", description: "This is movie two", rating: 5 },
+];
